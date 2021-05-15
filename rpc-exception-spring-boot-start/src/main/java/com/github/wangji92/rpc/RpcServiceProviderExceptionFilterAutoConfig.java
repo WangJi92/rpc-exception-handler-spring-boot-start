f6fd502e7de5aeeb4e4cdaffb2f1162b5859363f
wangji <983433479@qq.com>
2021-05-15 18:02:42 +0800

    fix

--- rpc-exception-spring-boot-start/src/main/java/com/github/wangji92/rpc/RpcServiceProviderExceptionFilterAutoConfig.java
+++ rpc-exception-spring-boot-start/src/main/java/com/github/wangji92/rpc/RpcServiceProviderExceptionFilterAutoConfig.java
@@ -2,6 +2,7 @@ package com.github.wangji92.rpc;
 
 import com.github.wangji92.rpc.annotation.DefaultRpcServiceHandlerExceptionResolver;
 import com.github.wangji92.rpc.constant.RpcServiceConstant;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,13 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableConfigurationProperties(RpcServiceProviderExceptionFilterProperties.class)
+@Slf4j
 public class RpcServiceProviderExceptionFilterAutoConfig {
 
     @Bean(name = RpcServiceConstant.RPC_SERVICE_HANDLER_EXCEPTION_RESOLVER_BEAN_NAME)
     @ConditionalOnProperty(prefix = "rpc.service.provider.exception", value = "enable", havingValue = "true", matchIfMissing = false)
     public DefaultRpcServiceHandlerExceptionResolver defaultRpcServiceHandlerExceptionResolver() {
+        log.info("rpc service exception filter start");
         return new DefaultRpcServiceHandlerExceptionResolver();
     }
 }

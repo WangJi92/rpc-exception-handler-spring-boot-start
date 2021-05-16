0032d5b7063c6f1ce38d9693eeec3a809bca3d5a
wangji <983433479@qq.com>
2021-05-16 15:04:58 +0800

    fix dubbo.registry.address=N/A  get dubbo server target is null

--- dubbo-exception-filter/src/main/java/com/github/wangji92/rpc/dubbo/utils/ProviderInvokerTargetUtils.java
+++ dubbo-exception-filter/src/main/java/com/github/wangji92/rpc/dubbo/utils/ProviderInvokerTargetUtils.java
@@ -23,16 +23,21 @@ public class ProviderInvokerTargetUtils {
      * @return
      */
     public static Object getServiceTarget(Invoker<?> invoker) {
-        if (!(invoker instanceof RegistryProtocol.InvokerDelegate)) {
+        // in filter invoker maybe  is RegistryProtocol.InvokerDelegate or DelegateProviderMetaDataInvoker
+        if (!(invoker instanceof RegistryProtocol.InvokerDelegate || invoker instanceof DelegateProviderMetaDataInvoker)) {
             return null;
         }
-        Invoker delegateProviderMetaDataInvokerObject = ((RegistryProtocol.InvokerDelegate) invoker).getInvoker();
-        if (!(delegateProviderMetaDataInvokerObject instanceof DelegateProviderMetaDataInvoker)) {
-            return null;
+        DelegateProviderMetaDataInvoker delegateProviderMetaDataInvoker = null;
+        if (invoker instanceof RegistryProtocol.InvokerDelegate) {
+            Invoker delegateProviderMetaDataInvokerObject = ((RegistryProtocol.InvokerDelegate) invoker).getInvoker();
+            if (!(delegateProviderMetaDataInvokerObject instanceof DelegateProviderMetaDataInvoker)) {
+                return null;
+            }
+            delegateProviderMetaDataInvoker = (DelegateProviderMetaDataInvoker) ((RegistryProtocol.InvokerDelegate) invoker).getInvoker();
+        } else {
+            // dubbo.registry.address=N/A
+            delegateProviderMetaDataInvoker = (DelegateProviderMetaDataInvoker) invoker;
         }
-
-        DelegateProviderMetaDataInvoker delegateProviderMetaDataInvoker = (DelegateProviderMetaDataInvoker) ((RegistryProtocol.InvokerDelegate) invoker).getInvoker();
-
         Field proxyInvokerField = ReflectionUtils.findField(delegateProviderMetaDataInvoker.getClass(), "invoker");
         if (proxyInvokerField == null) {
             return null;
